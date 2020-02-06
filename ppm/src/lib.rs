d3822a47bfd10ce0f419ca9f6728105f15c6d6bd
merzouk oumeddah <merzoukoumeddah@gmail.com>
2020-02-06 12:54:12 +0100

    first push

---
+++ ppm/src/lib.rs
@@ -0,0 +1,44 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct Pixel {
+    pub red: u8,
+    pub green: u8,
+    pub blue: u8,
+}
+
+impl Pixel {
+    pub fn new(red: u8, green: u8, blue: u8) -> Pixel {
+        Pixel {
+            red: red,
+            green: green,
+            blue: blue,
+        }
+    }
+}
+
+
+pub fn red(pix: Pixel)-> u8{
+    pix.red
+}
+pub fn green(pix: Pixel)-> u8{
+    pix.red
+}
+pub fn blue(pix: Pixel)-> u8{
+    pix.red
+}
+
+use std::fmt;
+
+impl fmt::Display for Pixel{
+    fn fmt(&self, f:&mut fmt:: Formatter<'_>) -> fmt::Result {
+        write!(f,"(r:{}, g:{}, b:{})", self.red, self.green, self.blue)
+
+    }
+}
